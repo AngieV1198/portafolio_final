2f36280230f0c2201e2ed04cac51b0b4362853a9
AngieV1198 <angievarela1998@gmail.com>
2022-05-09 10:05:25 -0500

    asas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import UX from "Components/UX/UX";
+import UX from "Components/Ux/UX";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home/Home";
